f000d811c132a9259880a2adbef5354c9d0e2303
NitinSeelan <nitinseelan53@gmail.com>
2024-10-27 14:09:46 -0700

    youtube

--- app/page.js
+++ app/page.js
@@ -21,8 +21,24 @@ export default function Home() {
           <br/>
           <MyLink link="https://www.youtube.com/@nitinseelan" text="Nitin Seelan"/>
         </div>
+        <div className="row">
+        <div className="column">
+          <h2>Nitin Seelan Tech</h2>
+          <iframe width="420" height="315"src="https://youtu.be/wUsXJ6bRtGc?autoplay=1&mute=1"></iframe>
+        </div>
+        <div className="column">
+          <h2>About</h2>
+          <p>lorem ipsum</p>
+        </div>
+        <div className="column">
+          <h2>My Other Sites and Channels</h2>
+          <MyLink link="https://www.youtube.com/@nitinseelantech" text="Nitin Seelan Tech"/>
+          <br/>
+          <MyLink link="https://www.youtube.com/@nitinseelan" text="Nitin Seelan"/>
+        </div>
       </div>
       <f1>Copyright Nitin Seelan 2024</f1>
     </div>
-  );
+    </div>
+    );
 }
