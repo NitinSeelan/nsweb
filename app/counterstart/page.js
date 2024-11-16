ee27f2fd2e20b2fffc6f5ad48297fc037ccca232
NitinSeelan <nitinseelan53@gmail.com>
2024-11-16 08:02:47 -0800

    build

--- app/counterstart/page.js
+++ app/counterstart/page.js
@@ -5,14 +5,6 @@ import MyLink from "../components/MyLink";
 export default function Home() {
   return (
     <div className="mainDiv">
-      {/* Links to preconnect to Google Fonts */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-      />
-
-
       <div className="row">
       
 
